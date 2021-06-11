be1f1ffc917906e3be992f1c678c6bb8cabee083
wonho <wonho951@gmail.com>
2021-06-11 17:53:57 +0900

    2021.06.11 기본API과제

---
+++ src/com/javaex/ex01/LastNumberApp.java
@@ -0,0 +1,28 @@
+package com.javaex.ex01;
+
+public class LastNumberApp {
+
+	public static void main(String[] args) {
+
+		// 수정하지 마세요
+		lastValue();
+	}
+
+
+	//런타임시 오류발생
+	public static void lastValue() {
+
+		int[] intArray = new int[3];
+		intArray[0] = 3;
+		intArray[1] = 6;
+		intArray[2] = 9;
+
+		int result;
+		
+		//아래부분을 try~catch문을 사용하여 수정하세요
+		result = intArray[3];
+		System.out.println(result);
+		
+	}
+
+}
---
+++ src/com/javaex/ex02/FileApp.java
@@ -0,0 +1,20 @@
+package com.javaex.ex02;
+
+import java.io.IOException;
+
+public class FileApp {
+
+	public static void main(String[] args) {
+
+		// try~catch문을 사용하여 에러를 수정하세요
+		fileRead();
+
+	}
+
+	// 수정하지 마세요
+	public static void fileRead() throws IOException{
+		throw new IOException(); // 강제로 예외 발생
+	}
+}
+
+

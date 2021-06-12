33a89f371db0de7da32b2411cb2c32420f14bfa1
wonho <wonho951@gmail.com>
2021-06-12 17:24:25 +0900

    2021.06.12 예외 기본API 과제

--- src/com/javaex/ex05/CalcApp.java
+++ src/com/javaex/ex05/CalcApp.java
@@ -6,9 +6,17 @@ public class CalcApp {
 
     public static void main(String[] args) {
 
-    	
+    	int a= 0;
+    	int b= 0;
     	Scanner sc = new Scanner(System.in);
-    	System.out.println(">>");
+    	System.out.print(">>");
+    	int setValue = sc.nextInt();
+    	
+    	
+    	if (setValue == a+b) {
+    		System.out.println("ej");
+    	}
+    	
     	
     	
     	sc.close();

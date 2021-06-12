4962a06de56cd6e27b8d7d11e8cbdfa4718539f1
wonho <wonho951@gmail.com>
2021-06-12 16:21:29 +0900

    2021.06.12 예외 기본API 과제

--- src/com/javaex/ex05/CalcApp.java
+++ src/com/javaex/ex05/CalcApp.java
@@ -1,8 +1,16 @@
 package com.javaex.ex05;
 
+import java.util.Scanner;
+
 public class CalcApp {
 
     public static void main(String[] args) {
 
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println(">>");
+    	
+    	
+    	sc.close();
+    	
     }
 }

6af01d80a70349a28fd1a50b73f30987417a95d1
wonho <wonho951@gmail.com>
2021-06-12 16:44:04 +0900

    2021.06.12 예외 기본API 과제

--- src/com/javaex/ex04/SumApp.java
+++ src/com/javaex/ex04/SumApp.java
@@ -8,14 +8,14 @@ public class SumApp {
 		
 		int sum = 0;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("숫자를 공백으로 구분해서 입력하세요");
+		System.out.println("숫자를 공백으로 구분해서 입력하세요");	//공백구분 해야하니까 split?
 		String numLine = sc.nextLine();
 
 		//코드를 작성하세요
 		
 		sum += Integer.parseInt(numLine);
 		
-
+		String [] sArray = numLine.split(" ");
 		
 		
 		System.out.println("합:" + sum);

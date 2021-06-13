a89b0e7ffb9ccb371bd153212dd241264a4b5a23
wonho <wonho951@gmail.com>
2021-06-13 19:43:24 +0900

    2021.06.13 예외 기본API 과제 (5번 미해결)

--- src/com/javaex/ex05/CalcApp.java
+++ src/com/javaex/ex05/CalcApp.java
@@ -9,15 +9,52 @@ public class CalcApp {
     	
     	Scanner sc = new Scanner(System.in);
     	  	
-    	int a = 0;
-    	int b = 0;
-		System.out.print(">>");
-		int num = sc.nextInt();
 
-    	if() {
-    		
+    	while (true) {
+			System.out.print(">>"); // 반복시켜줌
+			
+			String cal = sc.nextLine();
+			String[] cArray = cal.split(" ");
+			int a = Integer.parseInt(cArray[0]);	//a를 cArray의 0번방에 배치
+			int b = Integer.parseInt(cArray[2]);	//b를 cArray의 2번방에 배치
+			
+			
+			try {
+				if (cArray[1].equals("+")) { 
+					Add add = new Add();
+					add.setValue(a, b);
+					System.out.println(">>" + add.calculate());
+					
+					continue;
+				} else if (cArray[1].equals("-")) { 
+					Sub sub = new Sub();
+					sub.setValue(a, b);
+					System.out.println(">>" + sub.calculate());
+					
+					continue;
+				} else if (cArray[1].equals("*")) { 
+					Mul mul = new Mul();
+					mul.setValue(a, b);
+					System.out.println(">>" + mul.calculate());
+					continue;
+					
+				} else if (cArray[1].equals("/")) { 
+					Div div = new Div();
+					div.setValue(a, b);
+					System.out.println(">>" + div.calculate());
+										
+				} else if(cArray[1].equals("&")){
+					System.out.println("알 수 없는 연산입니다.");
+					break;
+				}
+			} catch (NumberFormatException e) {
+				System.out.println("알수 없는 연산입니다.");
+			}
+				
+			
+				
     	}
-    		
+    	    		
     		
     
     	

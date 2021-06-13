0a0db4d6d5610e2df43726abf397411d7672dc71
wonho <wonho951@gmail.com>
2021-06-13 17:09:14 +0900

    2021.06.12 예외 기본API 과제

--- src/com/javaex/ex08/Friend.java
+++ src/com/javaex/ex08/Friend.java
@@ -2,9 +2,24 @@ package com.javaex.ex08;
 
 public class Friend {
 	//필드 
+    private String friend;
     
-    
-    // getter/setter 작성
+    // 생성자
+	public Friend() {
+	}
+
+	public Friend(String friend) {
+		this.friend = friend;
+	}
+	// getter/setter 작성
+
+	public String getFriend() {
+		return friend;
+	}
+
+	public void setFriend(String friend) {
+		this.friend = friend;
+	}
 	
 	
 	//메소드-일반

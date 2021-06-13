ca7c4a9fb3bc41cdaa9e2b23f32d60919eb6ede0
wonho <wonho951@gmail.com>
2021-06-13 17:17:25 +0900

    2021.06.12 예외 기본API 과제

--- src/com/javaex/ex08/Friend.java
+++ src/com/javaex/ex08/Friend.java
@@ -2,27 +2,39 @@ package com.javaex.ex08;
 
 public class Friend {
 	//필드 
-    private String friend;
+    private String name;
+    private String hp;
+    private String school;
+
     
-    // 생성자
-	public Friend() {
+    // getter/setter 작성
+	public String getName() {
+		return name;
 	}
 
-	public Friend(String friend) {
-		this.friend = friend;
-	}
-	// getter/setter 작성
-
-	public String getFriend() {
-		return friend;
+	public void setName(String name) {
+		this.name = name;
 	}
 
-	public void setFriend(String friend) {
-		this.friend = friend;
+	public String getHp() {
+		return hp;
 	}
-	
+
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+
+	public String getSchool() {
+		return school;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
 	
 	//메소드-일반
-	
+	public void showInfo(){
+        System.out.println("이름:"+name+"  핸드폰:"+hp+"  학교:"+school);
+    }
 
 }
--- src/com/javaex/ex08/FriendApp.java
+++ src/com/javaex/ex08/FriendApp.java
@@ -8,11 +8,11 @@ public class FriendApp {
 
         
         Scanner sc = new Scanner(System.in);
-
+        //친구 입력하라고 출력하고
         System.out.println("친구를 3명 등록해 주세요");
 
         //친구 1,2,3 정보입력
-
+        //이름 + 핸드폰 + 학교 정보 나오게함
         
         
         
